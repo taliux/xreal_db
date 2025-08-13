2a16d2c716e749a06ae28cdea0285fb995e852b9
taliux <zrwang@gmail.com>
2025-08-13 16:34:02 +0800

    init es/mysql; connection w./w.o SSL

--- src/main/java/com/xreal/db/config/CustomElasticsearchRepositoryConfig.java
+++
@@ -1,7 +0,0 @@
-package com.xreal.db.config;
-
-// This configuration has been merged into ElasticsearchConfig
-// Keeping empty class to avoid breaking imports
-@Deprecated
-public class CustomElasticsearchRepositoryConfig {
-}
